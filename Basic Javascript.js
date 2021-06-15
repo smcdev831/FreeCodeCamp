82dd843dd6001a9abd240b51a0fce8f53c1a8d7e
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-06-14 21:16:59 -0500

    Update Basic Javascript.js

--- Basic Javascript.js
+++ Basic Javascript.js
@@ -9,3 +9,14 @@ a = 7;
 var b;
 b = a;
 // Only change code below this line
+
+
+// Only change code below this line
+var a =0;
+var b = 0;
+var c = "";
+// Only change code above this line
+
+a = a + 6;
+b = b + 15;
+c = c + "I am a String!";
