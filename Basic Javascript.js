a8d5ef1621e992e3d6f42195eb2297d614b1194a
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-06-18 18:46:18 -0500

    Update Basic Javascript.js

--- Basic Javascript.js
+++ Basic Javascript.js
@@ -29,3 +29,13 @@ var c = "I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
