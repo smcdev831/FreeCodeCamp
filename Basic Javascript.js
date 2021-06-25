de28eebd174637d142bc2b1f64540f8e98e4bbec
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-06-24 22:25:41 -0500

    Update Basic Javascript.js

--- Basic Javascript.js
+++ Basic Javascript.js
@@ -119,3 +119,8 @@ var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAd
 var myArray = [50,60,70];
 
 var myData = myArray[0];
+
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2][1];
