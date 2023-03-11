2d4af0d1ad43c2a43c9a4b0b0a7e9a4648ce5cb7
smcdev <smcdev@outlook.com>
2023-03-11 17:47:29 -0600

    Update Basic JavaScript.js

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -0,0 +1,46 @@
+// This is a Comment
+
+/* This is a multi-line
+comment */
+
+var myName;
+
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+var a = 9;
+
+var myFirstName = "Maria";
+var myLastName = "Moreno";
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
