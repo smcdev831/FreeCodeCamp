576fd7fa3509a2255b1986f0eb3b13a9e47d2dff
smcdev <smcdev@outlook.com>
2023-03-11 18:58:09 -0600

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -44,3 +44,36 @@ titleCaseOver = 9000;
 
 let catName = "Oliver";
 let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+const myDecimal = 7.7;
+
+const product = 2.0 * 2.5;
+
+const quotient = 4.4 / 2.0; // Change this line
