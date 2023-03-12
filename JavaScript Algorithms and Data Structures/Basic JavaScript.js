ca0b1d54b04846778d6278621cbef752fab2e913
smcdev <smcdev@outlook.com>
2023-03-12 18:47:44 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -263,3 +263,34 @@ function reusableFunction() {
 }
 
 reusableFunction();
+
+function functionWithArgs(x, y) {
+  console.log(x + y);
+}
+
+functionWithArgs(1, 2);
+
+function timesFive(x) {
+  return x * 5;
+}
+
+// Declare the myGlobal variable below this line
+
+let myGlobal = 10;
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
