02efe287c06cd06afc0a1192e8d26ec7b8604930
smcdev <smcdev@outlook.com>
2023-03-11 19:20:52 -0600

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -77,3 +77,47 @@ const myDecimal = 7.7;
 const product = 2.0 * 2.5;
 
 const quotient = 4.4 / 2.0; // Change this line
+
+const remainder = 11 % 3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
