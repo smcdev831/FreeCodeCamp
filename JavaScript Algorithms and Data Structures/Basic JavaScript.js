dc7c020340ba6316b49ce597dd23a9348051041a
smcdev <smcdev@outlook.com>
2023-03-11 19:58:08 -0600

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -121,3 +121,94 @@ const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+// Only change code below this line
+const myName = "Maria";
+const myStr = "My name is " + myName + " and I am well!";
+
+// Change code below this line
+const someAdjective = "awesome";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =
+  "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+// Only change code below this line
+const myArray = ["John", 7];
+
+// Only change code below this line
+const myArray = [
+  ["John", 7],
+  ["Maria", 831],
+];
+
+const myArray = [50, 60, 70];
+
+let myData = myArray[0];
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
