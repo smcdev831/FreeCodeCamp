0fa7b2a265c66fc14cc18e9ff3db024b35afc801
smcdev <smcdev@outlook.com>
2023-03-14 01:18:08 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -584,3 +584,37 @@ function testSize(num) {
 }
 
 testSize(7);
+
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes === 1) {
+    return names[0];
+  } else if (par - strokes >= 2) {
+    return names[1];
+  } else if (par - strokes === 1) {
+    return names[2];
+  } else if (par === strokes) {
+    return names[3];
+  } else if (strokes - par === 1) {
+    return names[4];
+  } else if (strokes - par === 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
