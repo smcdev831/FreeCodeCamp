ec191d2d88b46c319a5df95d7341265dc6626d8f
smcdev <smcdev@outlook.com>
2023-03-13 23:16:45 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -344,3 +344,27 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7);
+
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  let removed = arr.shift();
+  return removed;
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
