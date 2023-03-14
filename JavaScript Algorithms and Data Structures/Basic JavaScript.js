700c0ac60eda2ed443874488ffbaa680436f0ff1
smcdev <smcdev@outlook.com>
2023-03-13 22:32:33 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -294,3 +294,53 @@ function fun2() {
   }
   console.log(output);
 }
+
+function myLocalScope() {
+  // Only change code below this line
+  let myVar = "John";
+  console.log("inside myLocalScope", myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log("outside myLocalScope", myVar);
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  let outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
