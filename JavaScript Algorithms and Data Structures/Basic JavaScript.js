d2c1c999446ab9fc300d358a63a81dba4f5970ea
smcdev <smcdev@outlook.com>
2023-03-16 19:17:52 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -798,3 +798,46 @@ cc(3);
 cc(7);
 cc("K");
 cc("A");
+
+const myDog = {
+  // Only change code below this line
+  name: "Sylvanas",
+  legs: 4,
+  tails: 1,
+  friends: ["Jack", "Renee"],
+
+  // Only change code above this line
+};
+
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
+};
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water",
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"]; // Change this line
+const drinkValue = testObj["the drink"]; // Change this line
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas",
+};
+
+// Only change code below this line
+const playerNumber = 16; // Change this line
+const player = testObj[playerNumber]; // Change this line
