b92037c7d4b8a01571e31ac01bdf9dde54d003c7
smcdev <smcdev@outlook.com>
2023-03-16 19:11:16 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -757,3 +757,44 @@ function abTest(a, b) {
 }
 
 abTest(2, 2);
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count += 0;
+      break;
+    case 10:
+    case "J":
+    case "K":
+    case "Q":
+    case "A":
+      count -= 1;
+      break;
+  }
+
+  if (count > 0) {
+    return `${count} Bet`;
+  } else {
+    return `${count} Hold`;
+  }
+  // Only change code above this line
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
