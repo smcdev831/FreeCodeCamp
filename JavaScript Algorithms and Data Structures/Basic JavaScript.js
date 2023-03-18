4feee7d51f4adc957fa8fcb1e674a9cd10e1c389
smcdev <smcdev@outlook.com>
2023-03-17 22:12:30 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -967,3 +967,46 @@ function updateRecords(records, id, prop, value) {
   }
   return records;
 }
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
