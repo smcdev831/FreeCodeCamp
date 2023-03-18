43bde31008e969e74a60f09e7018636ffc19b5de
smcdev <smcdev@outlook.com>
2023-03-17 21:15:35 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -896,3 +896,14 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+  // Only change code above this line
+}
