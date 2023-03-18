97db24f298653f0f966850c429a6a282a070afeb
smcdev <smcdev@outlook.com>
2023-03-17 21:53:57 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -968,20 +968,3 @@ function updateRecords(records, id, prop, value) {
 
   return records;
 }
-
-function updateRecords(records, id, prop, value) {
-  if (prop !== "tracks" && value !== "") {
-    records[id][prop] = value;
-  } else if (
-    prop === "tracks" &&
-    value === "" &&
-    records[id].hasOwnProperty("tracks") === false
-  ) {
-    records[id][prop] = [value];
-  } else if (prop === "tracks" && value !== "") {
-    records[id][prop].push(value);
-  } else if (value === "") {
-    delete records[id][prop];
-  }
-  return records;
-}
