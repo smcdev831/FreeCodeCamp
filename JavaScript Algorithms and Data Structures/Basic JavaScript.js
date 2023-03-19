bc9df1565ea0dca45453c38f6aecb8642aaf46e3
smcdev <smcdev@outlook.com>
2023-03-19 18:50:07 -0500

    Update Basic JavaScript.js

--- JavaScript Algorithms and Data Structures/Basic JavaScript.js
+++ JavaScript Algorithms and Data Structures/Basic JavaScript.js
@@ -1093,3 +1093,47 @@ function lookUpProfile(name, prop) {
 }
 
 lookUpProfile("Akira", "likes");
+
+function randomFraction() {
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+
+function randomWholeNum() {
+  // Only change code below this line
+
+  return Math.floor(Math.random() * 10);
+}
+
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  // Only change code above this line
+}
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+
+function checkSign(num) {
+  return num === 0 ? "zero" : num > 0 ? "positive" : "negative";
+}
+
+checkSign(10);
