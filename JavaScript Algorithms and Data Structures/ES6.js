7d946a5d0d407c78ddcf57f5744c3f7ecd48b29a
smcdev <smcdev@outlook.com>
2023-03-19 19:20:28 -0500

    Create ES6.js

---
+++ JavaScript Algorithms and Data Structures/ES6.js
@@ -0,0 +1,36 @@
+function checkScope() {
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s.sort();
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
