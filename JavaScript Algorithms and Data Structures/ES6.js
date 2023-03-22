9c2139aeb89c885c4f332b9a443c8975f9277be9
smcdev <smcdev@outlook.com>
2023-03-21 22:46:42 -0500

    Update ES6.js

--- JavaScript Algorithms and Data Structures/ES6.js
+++ JavaScript Algorithms and Data Structures/ES6.js
@@ -56,3 +56,22 @@ const sum = (...args) => {
   }
   return total;
 };
+
+const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+let arr2;
+
+arr2 = [...arr1]; // Change this line
+
+console.log(arr2);
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80,
+};
+
+// Only change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
