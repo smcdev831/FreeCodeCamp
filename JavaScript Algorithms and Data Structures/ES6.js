e2839d1aa2f70d510d96ff3af001aea87489a246
smcdev <smcdev@outlook.com>
2023-03-21 22:41:06 -0500

    Update ES6.js

--- JavaScript Algorithms and Data Structures/ES6.js
+++ JavaScript Algorithms and Data Structures/ES6.js
@@ -48,3 +48,12 @@ console.log(myConcat([1, 2], [3, 4, 5]));
 // Only change code below this line
 const increment = (number, value = 1) => number + value;
 // Only change code above this line
+
+const sum = (...args) => {
+  const args = [x, y, z];
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+};
