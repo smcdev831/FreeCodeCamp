b344f3a40dfff892c9f43d400879ba771fc71e09
smcdev <smcdev@outlook.com>
2023-03-23 23:28:30 -0500

    Update Regular Expressions.js

--- JavaScript Algorithms and Data Structures/Regular Expressions.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions.js
@@ -13,3 +13,7 @@ let result = petRegex.test(petString);
 let myString = "freeCodeCamp";
 let fccRegex = /freeCodeCamp/i; // Change this line
 let result = fccRegex.test(myString);
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(/coding/); // Change this line
