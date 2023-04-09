2d1bcaf756c487d3391a53db1363149f88b51450
smcdev <smcdev@outlook.com>
2023-04-09 00:22:45 -0500

    Update Regular Expressions.js

--- JavaScript Algorithms and Data Structures/Regular Expressions.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions.js
@@ -30,3 +30,11 @@ let quoteSample =
   "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
