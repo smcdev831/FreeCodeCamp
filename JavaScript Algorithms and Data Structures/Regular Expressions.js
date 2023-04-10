a92c0ecba5732a9f63dd5ab01ea1291f373b8d86
smcdev <smcdev@outlook.com>
2023-04-10 00:08:49 -0500

    Update Regular Expressions.js

--- JavaScript Algorithms and Data Structures/Regular Expressions.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions.js
@@ -97,3 +97,15 @@ let result = sample.match(countNonWhiteSpace);
 let ohStr = "Ohhh no";
 let ohRegex = /Oh{3,6}\sno/; // Change this line
 let result = ohRegex.test(ohStr);
+
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/; // Change this line
+let result = haRegex.test(haStr);
+
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // Change this line
+let result = timRegex.test(timStr);
+
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
