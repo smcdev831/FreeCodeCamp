d0a7c08668c85717b233f0df2a18f5abdc9c1259
smcdev <smcdev@outlook.com>
2023-04-11 22:26:59 -0500

    Update Basic Data Structures.js

--- JavaScript Algorithms and Data Structures/Basic Data Structures.js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures.js
@@ -5,3 +5,12 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = 7;
 // Only change code above this line
 console.log(myArray);
+
+function mixedNumbers(arr) {
+  // Only change code below this line
+  arr.unshift("I", 2, "three");
+  arr.push(7, "VIII", 9);
+  // Only change code above this line
+  return arr;
+}
+console.log(mixedNumbers(["IV", 5, "six"]));
