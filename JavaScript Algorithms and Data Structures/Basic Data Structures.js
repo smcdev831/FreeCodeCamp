a1211a20e602730cb1ba4d59a8a435c1cbfb8a37
smcdev <smcdev@outlook.com>
2023-04-11 22:23:53 -0500

    Update Basic Data Structures.js

--- JavaScript Algorithms and Data Structures/Basic Data Structures.js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures.js
@@ -1,1 +1,7 @@
 let yourArray = [7, 831, "John", "Maria", true]; // Change this line
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 7;
+// Only change code above this line
+console.log(myArray);
