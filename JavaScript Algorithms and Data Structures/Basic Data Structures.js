fef9b95fd933c0aadd9cce161c51404c66e4542d
smcdev <smcdev@outlook.com>
2023-04-12 20:14:02 -0500

    Update Basic Data Structures.js

--- JavaScript Algorithms and Data Structures/Basic Data Structures.js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures.js
@@ -14,3 +14,32 @@ function mixedNumbers(arr) {
   return arr;
 }
 console.log(mixedNumbers(["IV", 5, "six"]));
+
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
+}
+console.log(popShift(["challenge", "is", "not", "complete"]));
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1, 4);
+// Only change code above this line
+console.log(arr);
+
+function htmlColorNames(arr) {
+  // Only change code below this line
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  // Only change code above this line
+  return arr;
+}
+console.log(
+  htmlColorNames([
+    "DarkGoldenRod",
+    "WhiteSmoke",
+    "LavenderBlush",
+    "PaleTurquoise",
+    "FireBrick",
+  ])
+);
