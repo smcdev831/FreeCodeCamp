4aca82088c45a8b392dc8b3cfd1958d1b7a506a6
smcdev <smcdev@outlook.com>
2023-04-14 21:30:34 -0500

    Update Basic Algorithm Scripting.js

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting.js
@@ -8,3 +8,12 @@ function reverseString(str) {
   return str.split("").reverse().join("");
 }
 reverseString("hello");
+
+function factorialize(num) {
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
+factorialize(5);
