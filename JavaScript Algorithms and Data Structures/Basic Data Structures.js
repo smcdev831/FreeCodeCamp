5f404a925d216cd59f21c76bb7e33d09ac6a60d6
smcdev <smcdev@outlook.com>
2023-04-14 21:12:00 -0500

    Update Basic Data Structures.js

--- JavaScript Algorithms and Data Structures/Basic Data Structures.js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures.js
@@ -115,3 +115,15 @@ let myNestedArray = [
   ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]],
   // Only change code above this line
 ];
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+};
+// Only change code below this line
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+// Only change code above this line
+console.log(foods);
