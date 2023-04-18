426d58ad109fad0f3851ad336d3586fbf44df63a
smcdev <smcdev@outlook.com>
2023-04-17 22:43:44 -0500

    Update Basic Algorithm Scripting.js

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting.js
@@ -57,3 +57,8 @@ largestOfFour([
   [32, 35, 37, 39],
   [1000, 1001, 857, 1],
 ]);
+
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target;
+}
+confirmEnding("Bastian", "n");
