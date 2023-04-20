e869e11a0b91c2d66c5685cf6d818cc725b63f00
smcdev <smcdev@outlook.com>
2023-04-20 17:32:16 -0500

    Update Basic Algorithm Scripting.js

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting.js
@@ -67,3 +67,25 @@ function repeatStringNumTimes(str, num) {
   return num > 0 ? str + repeatStringNumTimes(str, num - 1) : "";
 }
 repeatStringNumTimes("abc", 3);
+
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+  return undefined;
+}
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
