6f648fbac112bdd52fd2040eab48be04a4c7d145
smcdev <smcdev@outlook.com>
2023-04-22 16:33:43 -0500

    Update Object Oriented Programming.js

--- JavaScript Algorithms and Data Structures/Object Oriented Programming.js
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming.js
@@ -30,3 +30,24 @@ let dog = {
   },
 };
 dog.sayLegs();
+
+function Dog() {
+  this.name = "Sylvanas";
+  this.color = "black";
+  this.numLegs = 4;
+}
+
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
+
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+let terrier = new Dog("Jack", "brown");
