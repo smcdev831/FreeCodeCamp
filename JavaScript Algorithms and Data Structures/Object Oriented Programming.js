0de22119d33803248dd7caf033cb4e3f110cf432
smcdev <smcdev@outlook.com>
2023-04-21 21:47:43 -0500

    Create Object Oriented Programming.js

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming.js
@@ -0,0 +1,14 @@
+let dog = {
+  name: "Sylvanas",
+  numLegs: 4,
+};
+
+console.log(dog.name);
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+};
+// Only change code below this line
+console.log(dog.name);
+console.log(dog.numLegs);
